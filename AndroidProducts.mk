e9e48be4eaa699abb112e6a2a574d82f36c633e3
iamabdullahzaman <95879884+iamabdullahzaman@users.noreply.github.com>
2021-12-10 00:52:40 +0600

    santoni: Added xdroid

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,8 @@
 # AOSPA
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/pa_santoni.mk
+    $(LOCAL_DIR)/xdroid_santoni.mk
 
 COMMON_LUNCH_CHOICES := \
-    pa_santoni-userdebug \
-    pa_santoni-user \
-    pa_santoni-eng
+    xdroid_santoni-userdebug \
+    xdroid_santoni-user \
+    xdroid_santoni-eng
